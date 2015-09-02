a69076f717852e6cbff90a0c75eef22c8b6c71af
Piotr Król <piotr.krol@3mdeb.com>
2015-09-02 23:02:39 +0200

    uboot-scr: change naming convention and redesign boot flow for ml and sunxi

--- uboot-scr/boot.scr.ml
+++
@@ -1,7 +0,0 @@
-setenv bootargs "root=/dev/nfs init=/sbin/init nfsroot=192.168.1.8:/srv/nfs/cubietruck rw ip=dhcp console=ttyS0,115200 rootwait panic=10 consoleblank=0 debug"
-printenv bootargs
-tftp 0x46000000 ct-ml/zImage
-tftp 0x49000000 ct-ml/sun7i-a20-cubietruck.dtb
-env set fdt_high ffffffff
-bootz 0x46000000 - 0x49000000
-
