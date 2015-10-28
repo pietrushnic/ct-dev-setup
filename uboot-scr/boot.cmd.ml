468e03c6345372ceefeca7dfadf3e01de8f27d08
Piotr Król <piotr.krol@3mdeb.com>
2015-10-28 13:33:13 +0100

    uboot-scr/boot.cmd.ml: use NFSv4 and TCP

--- uboot-scr/boot.cmd.ml
+++ uboot-scr/boot.cmd.ml
@@ -1,6 +1,6 @@
 tftp 0x46000000 ct/ml/zImage
 tftp 0x49000000 ct/ml/sun7i-a20-cubietruck.dtb
-setenv bootargs "root=/dev/nfs init=/sbin/init nfsroot=${serverip}:/srv/nfs/ct/rootfs rw ip=dhcp console=ttyS0,115200 rootwait panic=10 consoleblank=0 debug"
+setenv bootargs "root=/dev/nfs init=/sbin/init nfsroot=${serverip}:/srv/nfs/ct/rootfs,vers=4,tcp rw ip=dhcp console=ttyS0,115200 rootwait panic=10 consoleblank=0 debug nfsrootdebug"
 env set fdt_high ffffffff
 bootz 0x46000000 - 0x49000000
 
